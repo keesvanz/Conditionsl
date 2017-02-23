4e0cbed5d8f6bd27b8f3580570bd5f8fc91523e5
keesvanz <kvanderaa@gmail.com>
2017-02-23 11:38:50 -0600

    [problem1 finnished]

---
+++ problem1.py
@@ -0,0 +1,13 @@
+print("enter int")
+
+firstnumber = int(raw_input())
+secondnumber = int(raw_input())
+thirdnumber = int(raw_input())
+
+if(firstnumber + secondnumber == thirdnumber):
+	print("true")
+
+
+if(firstnumber + secondnumber != thirdnumber):
+	print("false")
+
